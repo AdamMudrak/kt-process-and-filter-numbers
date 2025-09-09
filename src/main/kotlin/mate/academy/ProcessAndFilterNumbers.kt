9159f2e61d3142128f446284d7c85fdf2b202e09
AdamMudrak <mudrets2000@gmail.com>
2025-09-10 00:58:07 +0300

    Initial commit

--- src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
+++ src/main/kotlin/mate/academy/ProcessAndFilterNumbers.kt
@@ -1,5 +1,10 @@
 package mate.academy
+const val THRESHOLD = 25
+const val EVEN_DIVIDER = 2
+const val ODD_MULTIPLIER = 2
 
 fun processAndFilterNumbers(numbers: List<Int>): List<Int> {
-
+    return numbers
+        .map { if (it % 2 == 0) it / EVEN_DIVIDER else it * ODD_MULTIPLIER}
+        .filter { it > THRESHOLD }
 }
